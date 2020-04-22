08955ef1d4f085ad1c6b960cddd090f7fcd659a4
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-04-22 11:25:54 -0300

    Add Context Configurations

---
+++ MusicApi.Data/Configurations/MusicConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicApi.Core;
+using MusicApi.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicApi.Data.Configurations
+{
+    public class MusicConfiguration : IEntityTypeConfiguration<Music>
+    {
+        public void Configure(EntityTypeBuilder<Music> builder)
+        {
+            builder
+                .HasKey(m => m.Id);
+
+            builder
+                .Property(m => m.Id)
+                .UseIdentityColumn();
+
+            builder
+                .Property(m => m.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .HasOne(m => m.Artist)
+                .WithMany(a => a.Musics)
+                .HasForeignKey(m => m.ArtistId);
+
+            builder
+                .ToTable("Musics");
+        }
+    }
+}

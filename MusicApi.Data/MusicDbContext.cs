c028b379639e6ea2fcb21e9af9872956e98caf39
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-04-22 11:28:27 -0300

    Add Context

---
+++ MusicApi.Data/MusicDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApi.Core.Models;
+using MusicApi.Data.Configurations;
+
+namespace MusicApi.Data
+{
+    public class MusicDbContext : DbContext
+    {
+        public MusicDbContext(DbContextOptions<MusicDbContext> options)
+             : base(options) { }
+
+
+        public DbSet<Music> Musics { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .ApplyConfiguration(new MusicConfiguration());
+
+            builder
+                .ApplyConfiguration(new ArtistConfiguration());
+        }
+
+    }
+}

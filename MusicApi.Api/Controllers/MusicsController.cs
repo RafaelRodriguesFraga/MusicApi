a6c0324e419990c1edd63f45456d47370e683806
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-04-23 11:10:26 -0300

    Add Controller

---
+++ MusicApi.Api/Controllers/MusicsController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MusicApi.Api.Resources;
+using MusicApi.Core.Models;
+using MusicApi.Core.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MusicApi.Api.Controllers
+{
+
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicsController : ControllerBase
+    {
+
+        private readonly IMusicService _musicService;
+        private readonly IMapper _mapper;
+
+        public MusicsController(IMusicService musicService, IMapper mapper)
+        {
+            this._musicService = musicService;
+            this._mapper = mapper;
+        }
+        /// <summary>
+        /// Get All Musics
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Music>>> GetAllMusics()
+        {
+            var musics = await _musicService.GetAllWithArtist();
+            var musicResources = _mapper.Map<IEnumerable<Music>, IEnumerable<MusicResource>>(musics);
+            return Ok(musicResources);
+        }
+    }
+}

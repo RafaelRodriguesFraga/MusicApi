93dc31491573e9229619ceba99ed3b57848b793c
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-04-23 11:11:02 -0300

    Add AutoMapper

---
+++ MusicApi.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MusicApi.Api.Resources;
+using MusicApi.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi.Api.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+            //Domain to Resource
+            CreateMap<Music, MusicResource>();
+            CreateMap<Artist, ArtistResource>();
+
+            //Resource to Domain
+            CreateMap<MusicResource, Music>();
+            CreateMap<ArtistResource, Artist>();
+        }
+    }
+}

e35f1132a1821659e0faf188ae413d82c0a3f1ee
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-04-23 11:17:10 -0300

    Add Swagger

--- MusicApi.Api/Startup.cs
+++ MusicApi.Api/Startup.cs
@@ -1,11 +1,18 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using MusicApi.Core;
+using MusicApi.Core.Services;
 using MusicApi.Data;
+using MusicApi.Services;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace MusicApi.Api
 {
@@ -24,10 +31,26 @@ namespace MusicApi.Api
             services.AddControllers();
 
             services.AddScoped<IUnityOfWork, UnityOfWork>();
-            
+
+            services.AddTransient<IMusicService, MusicService>();
+            services.AddTransient<IArtistService, ArtistService>();
+
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddDbContext<MusicDbContext>(options => 
                     options.UseNpgsql(Configuration.GetConnectionString("Default"), 
                     x => x.MigrationsAssembly("MusicApi.Data")));
+
+            //Registrar o gerador do swagger definindo um ou mais document os Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Catalogo", Version = "v1" });
+
+                // Caminho para o Swagger JSON e UI
+                string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +70,16 @@ namespace MusicApi.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Habilita o Middleware para servir o SwaggerUI especificando o endpoint
+            app.UseSwagger();
+
+            //Habilita o Middleware para servir o SwaggerUI especificando o endpoint Swaggger JSON
+            app.UseSwaggerUI(c =>
+            {
+               
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Catálogo V1");
             });
         }
     }

34220ee22450419d3b083185901dab2419132363
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-04-23 11:17:37 -0300

    Add Services

---
+++ MusicApi.Services/ArtistService.cs
@@ -0,0 +1,48 @@
+﻿using MusicApi.Core;
+using MusicApi.Core.Models;
+using MusicApi.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApi.Services
+{
+    public class ArtistService : IArtistService
+    {
+        private readonly IUnityOfWork _unityOfWork;
+        public ArtistService(IUnityOfWork unityOfWork)
+        {
+
+        }
+
+        public async Task<Artist> CreateArtist(Artist newArtist)
+        {
+            await _unityOfWork.Artists.AddAsync(newArtist);
+
+            return newArtist;
+        }
+
+        public async Task DeleteArtist(Artist artist)
+        {
+            _unityOfWork.Artists.Remove(artist);
+            await _unityOfWork.CommitAsync();
+        }
+
+        public async Task<IEnumerable<Artist>> GetAllArtists()
+        {
+            return await _unityOfWork.Artists.GetAllAsync();
+        }
+
+        public async Task<Artist> GetArtistById(int id)
+        {
+            return await _unityOfWork.Artists.GetByIdAsync(id);
+        }
+
+        public async Task UpdateArtist(Artist artistToBeUpdated, Artist artist)
+        {
+            artistToBeUpdated.Name = artist.Name;
+            await _unityOfWork.CommitAsync();
+        }
+    }
+}
---
+++ MusicApi.Services/MusicService.cs
@@ -0,0 +1,55 @@
+﻿using MusicApi.Core;
+using MusicApi.Core.Models;
+using MusicApi.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApi.Services
+{
+    public class MusicService : IMusicService
+    {
+        private readonly IUnityOfWork _unityOfWork;
+        public MusicService(IUnityOfWork unityOfWork)
+        {
+            this._unityOfWork = unityOfWork;
+        }
+
+        public async Task<Music> CreateMusic(Music newMusic)
+        {
+            await _unityOfWork.Musics.AddAsync(newMusic);
+            await _unityOfWork.CommitAsync();
+            return newMusic;
+        }
+
+        public async Task DeleteMusic(Music music)
+        {
+            _unityOfWork.Musics.Remove(music);
+            await _unityOfWork.CommitAsync();
+        }
+
+        public async Task<IEnumerable<Music>> GetAllWithArtist()
+        {
+            return await _unityOfWork.Musics.GetAllWithArtistAsync();
+        }
+
+        public async Task<Music> GetMusicById(int id)
+        {
+            return await _unityOfWork.Musics.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Music>> GetMusicsByArtistId(int artistId)
+        {
+            return await _unityOfWork.Musics.GetAllWithArtistByArtistIdAsync(artistId);
+        }
+
+        public async Task UpdateMusic(Music musicToBeUpdated, Music music)
+        {
+            musicToBeUpdated.Name = music.Name;
+            musicToBeUpdated.ArtistId = music.ArtistId;
+
+            await _unityOfWork.CommitAsync();
+        }
+    }
+}

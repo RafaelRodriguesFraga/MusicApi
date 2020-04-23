a4bda9621d0649fabf092fbaeb59fae1a674078c
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-04-23 12:04:14 -0300

    Add Validators

---
+++ MusicApi.Api/Validators/SaveArtistResourceValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MusicApi.Api.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi.Api.Validators
+{
+    public class SaveArtistResourceValidator : AbstractValidator<SaveArtistResource>
+    {
+        public SaveArtistResourceValidator()
+        {
+            RuleFor(a => a.Name)
+                .NotEmpty()
+                .MaximumLength(50);
+        }
+    }
+}
